34cc17c5aad6f6281feed03281ba29be26b1bd4b
nvaccina <niccolovac@gmail.com>
2023-03-17 14:14:58 +0100

    HTML e JavaScript completato

--- js/scrtipt.js
+++ js/scrtipt.js
@@ -3,3 +3,19 @@
 2 Chiedi il suo cognome,
 3 Chiedi il suo colore preferito,
 4 Scrivi sulla pagina nomecognomecolorepreferito23
+*/
+
+const name = prompt("Inserisci il tuo nome");
+
+const surname = prompt("Inserisci il tuo cognome");
+
+const colore = prompt("Qual è il tuo colore preferito?");
+
+const age = prompt("Quanti anni hai?");
+
+const output = `
+Ciao ${name} ${surname} <br>
+Il tuo colore preferito è il ${colore} <br>
+Hai ${age} anni
+`
+document.getElementById("utente").innerHTML = output;
